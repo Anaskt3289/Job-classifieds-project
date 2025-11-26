1b3c4caabe89bb37e458169933cc7cb5aeb027bc
anaskt <anask@elixiamail.com>
2025-11-26 11:34:18 +0530

    removed cron

--- src/trigger/trigger.service.ts
+++ src/trigger/trigger.service.ts
@@ -8,16 +8,16 @@ export class TriggerService {
   constructor(
     private readonly httpService: HttpService
   ){}
-  @Cron('*/10 * * * *') // Runs every 10 minutes
-  handleCron() {
-    firstValueFrom(
-      this.httpService.get(`${process.env.OWN_SERVER_ALTERNATE_URL}/trigger/server/status`)
-    ).then((response)=>{
-      console.log('Cron job executed to keep server status :', response);
-    }).catch((err)=>{
-      console.log(`Error on cron to get server status ::`,err);
-    })
-  }
+  // @Cron('*/10 * * * *') // Runs every 10 minutes
+  // handleCron() {
+  //   firstValueFrom(
+  //     this.httpService.get(`${process.env.OWN_SERVER_ALTERNATE_URL}/trigger/server/status`)
+  //   ).then((response)=>{
+  //     console.log('Cron job executed to keep server status :', response);
+  //   }).catch((err)=>{
+  //     console.log(`Error on cron to get server status ::`,err);
+  //   })
+  // }
 
   async getTriggerStatus() {
     return {
